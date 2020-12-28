e9d4b3114e1a50000abcc9d7daa0e88decb521d2
Karol Stumski <stumski.karol@outlook.com>
2020-12-28 18:27:03 +0100

    update

--- lib/screens/qr_code_generator.dart
+++ lib/screens/qr_code_generator.dart
@@ -1,4 +1,5 @@
 import 'package:check_your_bmr_and_bmi/components/background_card.dart';
+import 'package:check_your_bmr_and_bmi/components/buttons.dart';
 import 'package:flutter/material.dart';
 import 'package:check_your_bmr_and_bmi/constants.dart';
 
@@ -24,28 +25,67 @@ class _QRCodeGeneratorState extends State<QRCodeGenerator> {
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Expanded(
+            flex: 1,
             child: BackgroundCard(
               childContainer: TextField(
                 onChanged: (text) {
                   print("First text field: $text");
                 },
                 style: TextStyle(color: Colors.black),
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
                 cursorColor: Colors.black,
                 decoration: InputDecoration(
                     border: InputBorder.none,
-                    hintText: 'Enter a text',
+                    hintText: 'Enter a title',
                     hintStyle: TextStyle(color: Colors.black26),
-                    focusedBorder: OutlineInputBorder(),
-                    disabledBorder: OutlineInputBorder(),
-                    enabledBorder: OutlineInputBorder(),
+                    focusedBorder: OutlineInputBorder(borderSide: BorderSide(),),
+                    disabledBorder: OutlineInputBorder(borderSide: BorderSide(),),
+                    enabledBorder: OutlineInputBorder(borderSide: BorderSide(),),
                 ),
 
               ),
             ),
           ),
+          Expanded(
+            flex: 3,
+            child: BackgroundCard(
+              childContainer: TextField(
+                autofocus: true,
+                onChanged: (text) {
+                  print("Second text field: $text");
+                },
+                style: TextStyle(color: Colors.black),
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                cursorColor: Colors.black,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter a URL or content',
+                  hintStyle: TextStyle(color: Colors.black26),
+                  focusedBorder: OutlineInputBorder(),
+                  disabledBorder: OutlineInputBorder(),
+                  enabledBorder: OutlineInputBorder(),
+                ),
+
+              ),
+            ),
+          ),
+          Expanded(
+              flex: 3,
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(image: NetworkImage('http://api.qrserver.com/v1/create-qr-code/?data=HelloWorld!&size=300x300'))
+                ),
+              ),
+          ),
+          Spacer(),
+          Expanded(flex: 1, child: Button(text: Text("Generate"))),
+          Expanded(flex: 1, child: Button(text: Text("Save"))),
+          Expanded(flex: 1, child: Button(text: Text("QR Gallery"))),
         ],
       ),
     );
